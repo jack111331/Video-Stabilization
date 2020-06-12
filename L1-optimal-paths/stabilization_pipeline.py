be1a1060a584bd1b963cbfb645b2bb7f437fffd6
ishank-juneja <ishankjuneja@gmail.com>
2020-06-12 13:02:14 +0530

    Remove ignored files

--- L1-optimal-paths/stabilization_pipeline.py
+++ L1-optimal-paths/stabilization_pipeline.py
@@ -14,13 +14,13 @@ w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
 # Define the codec for output video
-fourcc = cv.VideoWriter_fourcc(*'MJPG')
+fourcc = cv.VideoWriter_fourcc(*'MPEG')
 
 # Get input fps, use same for output
-fps = cap.get(cv.CAP_PROP_FPS)
+fps = int(cap.get(cv.CAP_PROP_FPS))
 
 # Set up output video stream
-out = cv.VideoWriter('video_out.mp4', fourcc, fps, (w, h))
+out = cv.VideoWriter('video_out.avi', fourcc, fps, (2*w, h))
 
 # Read first frame
 _, prev = cap.read()
@@ -76,4 +76,95 @@ for i in range(n_frames - 2):
 
     # print("Frame: " + str(i) + "/" + str(n_frames) + " -  Tracked points : " + str(len(prev_pts)))
 
-print(transforms)
+#print(transforms)
+
+####
+# Trajectory calculation, integrates changes to give current value of
+# x, y, theta
+#print(transforms.shape)
+trajectory = np.cumsum(transforms, axis=0)
+#print(trajectory.shape)
+# print(trajectory)
+
+
+def movingAverage(curve, radius):
+    window_size = 2 * radius + 1
+    # Define the filter
+    f = np.ones(window_size)/window_size
+    # Add padding to the boundaries
+    curve_pad = np.lib.pad(curve, (radius, radius), 'edge')
+    # Apply convolution
+    curve_smoothed = np.convolve(curve_pad, f, mode='same')
+    # Remove padding
+    curve_smoothed = curve_smoothed[radius:-radius]
+    # return smoothed curve
+    return curve_smoothed
+
+
+def smooth(trajectory):
+    smoothed_trajectory = np.copy(trajectory)
+    # Filter the x, y and angle curves
+    for i in range(3):
+        smoothed_trajectory[:,i] = movingAverage(trajectory[:, i], radius=2)
+
+    return smoothed_trajectory
+
+
+smoothed_trajectory = smooth(trajectory)
+
+# Calculate difference in smoothed_trajectory and trajectory
+difference = smoothed_trajectory - trajectory
+
+# Calculate newer transformation array
+transforms_smooth = transforms + difference
+
+
+def fixBorder(frame):
+    s = frame.shape
+    # Scale the image 4% without moving the center
+    T = cv.getRotationMatrix2D((s[1]/2, s[0]/2), 0, 1.04)
+    frame = cv.warpAffine(frame, T, (s[1], s[0]))
+    return frame
+
+
+# Reset stream to first frame
+cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+
+# Write n_frames-1 transformed frames
+for i in range(n_frames - 2):
+    # Read next frame
+    success, frame = cap.read()
+    if not success:
+        break
+
+    # Extract transformations from the new transformation array
+    dx = transforms_smooth[i, 0]
+    dy = transforms_smooth[i, 1]
+    da = transforms_smooth[i, 2]
+
+    # Reconstruct transformation matrix accordingly to new values
+    m = np.zeros((2, 3), np.float32)
+    m[0, 0] = np.cos(da)
+    m[0, 1] = -np.sin(da)
+    m[1, 0] = np.sin(da)
+    m[1, 1] = np.cos(da)
+    m[0, 2] = dx
+    m[1, 2] = dy
+
+    # Apply affine wrapping to the given frame
+    frame_stabilized = cv.warpAffine(frame, m, (w, h))
+
+    # Fix border artifacts
+    frame_stabilized = fixBorder(frame_stabilized)
+
+    # Write the frame to the file
+    frame_out = cv.hconcat([frame, frame_stabilized])
+
+    # If the image is too big, resize it.
+    # if frame_out.shape[1] > 1920:
+
+    frame_out = cv.resize(frame_out, (frame_out.shape[1], frame_out.shape[0]))
+
+    cv.imshow("Before and After", frame_out)
+    cv.waitKey(10)
+    out.write(frame_out)
