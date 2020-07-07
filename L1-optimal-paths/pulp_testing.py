b45a4bde05d31fa0efb6b9b22c2e3317fad8f789
ishank-juneja <ishankjuneja@gmail.com>
2020-07-07 13:22:04 +0530

    pulp testing script complete

--- L1-optimal-paths/pulp_testing.py
+++ L1-optimal-paths/pulp_testing.py
@@ -2,17 +2,34 @@ import numpy as np
 from pulp import *
 
 
-# A list of identifiers
-IDENTIFIERS = ['A', 'B', 'C', 'D', 'E']
-PRICES = dict(zip(IDENTIFIERS, [100.0, 99.0, 100.5, 101.5, 200.0]))
-n = len(IDENTIFIERS)
+def printProb(prob):
+    # Print out the parameters of the variables associated
+    # with the name and varValue attributes of each
+    # variable associated with the problem
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
+    # Print the status of the LPP solution
+    print("Status:", pulp.LpStatus[prob.status])
 
-x = LpVariable.dicts("e", indexs = IDENTIFIERS, lowBound=0, upBound=1, cat='Integer', indexStart=[])
-prob = pulp.LpProblem("Minimalist example", pulp.LpMaximize)
-prob += pulp.lpSum([x[i]*PRICES[i] for i in IDENTIFIERS]), " Objective is sum of prices of selected items "
-prob += 5, " Objective is sum of prices of selected items "
-prob += pulp.lpSum([x[i] for i in IDENTIFIERS]) == 2, " Constraint is that we choose two items "
+
+# A list of identifiers as strings
+names = ['A', 'B', 'C', 'D', 'E']
+# Create a dictionary out of these identifiers
+PRICES = dict(zip(names, [100.0, 99.0, 100.5, 101.5, 200.0]))
+# Number of variables
+n = len(names)
+# Create a list of LP variables objects from the list names
+x = LpVariable.dicts("e", indexs=names, lowBound=0, upBound=1)
+print(type(x))
+print(x)
+# Specify problem name and problem type
+prob = pulp.LpProblem("Example-Name", pulp.LpMinimize)
+# Use the variable list x and the price dictionary to assemble the objective
+prob += pulp.lpSum([x[i]*PRICES[i] for i in names])
+# Doing something like prob += 5 resets the problem objective
+# Statements with operators are automatically interpreted as constraints
+prob += pulp.lpSum([x[i] for i in names]) == 2.0
+# Compute solution
 prob.solve()
-for ident in IDENTIFIERS:
-    if x[ident] == 1:
-        print(ident + " is in the basket ")
+# Display the solution
+printProb(prob)
