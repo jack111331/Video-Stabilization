11966c2c45d52997d8261dbbdb65836b2f77a487
ishank-juneja <ishankjuneja@gmail.com>
2020-10-18 01:08:31 +0530

    Added option to plot and made function out of it

--- L1-optimal-paths/stabilization_L1_optimal.py
+++ L1-optimal-paths/stabilization_L1_optimal.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import matplotlib.pyplot as plt
 from lpp import stabilize
 import argparse
 
@@ -19,6 +20,37 @@ def get_corners(im_shape, crop_ratio):
     crop_y = round(img_ctr_y - crop_h / 2)
     # Return corner points of crop window
     return crop_x, crop_w + crop_x, crop_y, crop_h + crop_y
+
+
+# Function to plot the original and the corrected/stabilized
+# x and y components of the camera trajectory
+def plot_trajectory(og, stb, name):
+    # Print the trajectory and subsequently plot them
+    print("Original camera trajectory (x, y)")
+    print(og)
+    print("--------------------------------")
+    print("Stabilized camera trajectory (x, y)")
+    print(stb)
+    # x-coordinate trajectory
+    plt.figure()
+    plt.plot(og[:, 0])
+    plt.plot(stb[:, 0])
+    plt.xlabel('Frame Number')
+    plt.ylabel('2D Camera x coord. (pixels)')
+    plt.title('Original vs Stab x')
+    plt.legend(['Original', 'Stabilized'])
+    plt.savefig("plots/{0}_traj_x.png".format(name))
+    plt.close()
+    # y-coordinate trajectory
+    plt.figure()
+    plt.plot(og[:, 1])
+    plt.plot(stb[:, 1])
+    plt.xlabel('Frame Number')
+    plt.ylabel('2D Camera y coord. (pixels)')
+    plt.title('Original vs Stab y')
+    plt.legend(['Original', 'Stabilized'])
+    plt.savefig("plots/{0}_traj_y.png".format(name))
+    plt.close()
 
 
 def main(args):
@@ -99,50 +131,18 @@ def main(args):
         P_trajectory[i, :, :] = C_trajectory[i, :, :] @ B_transforms[i, :, :]
     # Starting coordinate (0, 0) in homogeneous system
     origin = np.array([0, 0, 1])
-    # Evolution of coordinate of camera trajectory under original scheme
-    evolution_og = origin @ C_trajectory
-    # Evolution of origin under stabilized trajectory
-    evolution_stab = origin @ P_trajectory
-    # Print and plot these trajectories
-    # print(evolution_og)
-    # print("--------------------------------")
-    # print(evolution_stab)
-    # x-coordinate trajectory
-    # plt.figure()
-    # plt.plot(evolution_og[:, 0])
-    # plt.plot(evolution_stab[:, 0])
-    # plt.title('Original vs Stab x')
-    # plt.legend(['Original', 'Stabilized'])
-    # plt.savefig("plots/" + in_name + "_traj_x.png")
-    # plt.close()
-    # y-coordinate trajectory
-    # plt.figure()
-    # plt.plot(evolution_og[:, 1])
-    # plt.plot(evolution_stab[:, 1])
-    # plt.title('Original vs Stab y')
-    # plt.legend(['Original', 'Stabilized'])
-    # plt.savefig("plots/" + in_name + "_traj_y.png")
-    # plt.close()
+    # if the plotting camera trajectory flag is passed, plot trajectories
+    if args.trajPlot:
+        # Evolution of coordinate of camera trajectory under original scheme
+        evolution_og = origin @ C_trajectory
+        # Evolution of origin under stabilized trajectory
+        evolution_stab = origin @ P_trajectory
+        plot_trajectory(evolution_og, evolution_stab, in_name)
     # Get frame limits, i.e the coordinates of the corners under the
     # crop-ratio passed to the program
     frame_limits = get_corners((w, h), crop_ratio)
     # print(frame_limits)
-    # Reset stream to first frame
-    # cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-    # Read next frame
-    # success, frame = cap.read()
-    # Also convert to stabilized set of frames
-    # frame_stabilized = cv.warpAffine(frame, B_transforms[0, :, :2].T, (w, h))
-    # Take centered window out of the stabilized window to avoid moving
-    # black corners that would inevitably created by the stabilization transform
-    # frame_stabilized = frame_stabilized[frame_limits[0]:frame_limits[1], frame_limits[2]:frame_limits[3]]
-    # Take out the same centered crop window from the original
-    # video for comparison between the original and the stabilized versions
-    # frame = frame[frame_limits[0]:frame_limits[1], frame_limits[2]:frame_limits[3]]
-    # Construct the first output frame by the horizontal concatenation of the
-    # Centered versions of the original and the stabilized frames
-    # frame_out = cv.hconcat([frame, frame_stabilized])
-    # Set up output video stream
+    # Set up output video stream, the shape order is (cols, rows) instead of the usual (rows, cols)
     out = cv.VideoWriter("results/{0}stb_L1optimal.avi".format(in_name),
                          fourcc, fps, (2*(frame_limits[3] - frame_limits[2]), frame_limits[1] - frame_limits[0]))
     # Reset stream to first frame
@@ -167,6 +167,7 @@ def main(args):
         # If the image is too big, resize it.
         # if frame_out.shape[1] > 1920:
         # frame_out = cv.resize(frame_out, (frame_out.shape[1], frame_out.shape[0]))
+        # Display the result in a window before writing it to file
         # cv.imshow("Before and After", frame_out)
         # cv.waitKey(10)
         out.write(frame_out)
@@ -178,9 +179,11 @@ def main(args):
 if __name__ == "__main__":
     # Pass command line inputs for the stabilization procedure
     parser = argparse.ArgumentParser()
-    # Input video file name
-    parser.add_argument("-i", action="store", dest="file") # Default type is string
+    # Add input file path, default type is string
+    parser.add_argument("-i", action="store", dest="file")
     parser.add_argument("-crop-ratio", action="store", dest="crop_ratio", type=float)
+    # Boolean argument that is True if the flag --trajPlot is passed
+    parser.add_argument("--trajPlot", action="store_true")
     # read cmd line arguments
     args_read = parser.parse_args()
     main(args_read)
